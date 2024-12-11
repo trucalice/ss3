d189ca3ad317da40863e72d569ad9521d6506e70
trucalice <nguyenthithanhhuong99992006@gmail.com>
2024-12-11 21:47:21 +0700

    Add files via upload

---
+++ bai 6.cpp
@@ -0,0 +1,12 @@
+# include<stdio.h>
+
+int main(){
+	int height, edgelength;
+    printf("nhap height: ");
+    scanf("%d",&height);
+	printf("nhap edge length: ");	
+	scanf("%d",&edgelength);
+    printf ("acreage: %d",height * edgelength / 2);
+    return 0;
+	
+}
