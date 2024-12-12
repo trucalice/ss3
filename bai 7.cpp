95c7366f71bc474c5bfd07a747053a986033cfb2
trucalice <nguyenthithanhhuong99992006@gmail.com>
2024-12-12 14:22:31 +0700

    Add files via upload

--- bai 7.cpp
+++ bai 7.cpp
@@ -5,20 +5,9 @@ int main() {
     char number[5];
     printf("Nhap so nguyen co 4 chu so: ");
     scanf("%4s", number);
-    while (getchar() != '\n');
-    if (strlen(number) != 4) {
-        printf("Ban phai nhap dung 4 chu so!\n");
-        return 1;
-    }
-    int total = 0;
-    for (int i = 0; i < 4; i++) {
-        if (number[i] < '0' || number[i] > '9') {
-            printf("Vui long chi nhap so nguyen!\n");
-            return 1;
-        }
-        total += number[i] - '0';
-}
-    printf("Tong cac chu so %s la: %d\n", number, total);
+    int n = (number[0] - '0') + (number[1] - '0') + (number[2] - '0') + (number[3] - '0');
+    printf("tong cac chu so %s la: %d\n", number, n);
+    
     return 0;
 }
 
